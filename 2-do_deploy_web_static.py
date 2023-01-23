13640271082709a9416bd1ab9db746f4de3734df
Otswag <106761383+Otswag@users.noreply.github.com>
2023-01-23 22:26:26 +0100

    Ready

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+""" Program that distributes an archive to your web servers,
+cusing the function do_deploy """
 from datetime import datetime
 from fabric.api import *
-import shlex
-import os
+from os import path
+
+env.hosts = ['52.72.8.186', '3.85.141.116']
 
 
-env.hosts = ['<52.72.8.186>', '3.85.141.116']
-env.user = "ubuntu"
+def do_pack():
+    """ Generates a .tgz archive from the contents of the web_static
+    folder of your AirBnB Clone repo """
+    date_str = datetime.now().strftime('%Y%m%d%H%M%S')
+    local("mkdir -p versions/")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(date_str))
+        return "versions/web_static_{}.tgz".format(date_str)
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """ Distributes an archive to the web servers """
+    if not path.exists(archive_path):
         return False
+    # split the path and get the second element in the list
+    file_path = archive_path.split("/")[1]
+    serv_folder = "/data/web_static/releases/" + file_path
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        run("sudo mkdir -p " + serv_folder)
+        run("sudo tar -xzf /tmp/" + file_path + " -C " + serv_folder + "/")
+        run("sudo rm /tmp/" + file_path)
+        run("sudo mv " + serv_folder + "/web_static/* " + serv_folder)
+        run("sudo rm -rf " + serv_folder + "/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s " + serv_folder + " /data/web_static/current")
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
